6dfdb158b16d946fafef3cebfbb47e678fc8c742
zoidburger <roman@salameh.de>
2025-02-15 12:20:55 +0100

    add enum example

--- src/examples/queryExamples.ts
+++ src/examples/queryExamples.ts
@@ -1,6 +1,7 @@
 import { User } from "edgeql-types/interfaces";
 import { db, runQuery } from "~/edgedb";
 import { Infer } from "edgeql-types/helper";
+import { USER_STATUS } from "edgeql-types/generated";
 
 type UserCreateData = Pick<User, "username" | "email">;
 // Use the exported type from edgeql-types/interfaces for types
@@ -16,6 +17,19 @@ function createUser(user: UserCreateData) {
 
 // We can infer the type of the object that gets returned, when the query is executed
 type InferredWithHelperFunction = Infer<ReturnType<typeof createUser>>;
-
 // The generically typed runQuery function also correctly infers the return type
 const user = runQuery(createUser(exampleUser));
+
+function updateUserStatus({ id, status }: Pick<User, "id" | "status">) {
+  return db.update(db.User, (user) => ({
+    set: {
+      status,
+    },
+    filter_single: { id },
+  }));
+}
+
+// We use our custom generated enum here and validate that update return values is inferred correctly
+const updatedUser = runQuery(
+  updateUserStatus({ id: user.id, status: USER_STATUS.verified })
+);
